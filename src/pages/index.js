1f15839d70ffd1b3d3db57a29cc00a0ba1fdb441
Graham Plata <graham.plata@gmail.com>
2018-12-25 17:29:21 -0500

    put placeholder back

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 const IndexPage = props => (
   <StaticQuery
@@ -17,7 +18,7 @@ const IndexPage = props => (
     `}
     render={data => (
       <Layout location={props.location}>
-        <p>1ßs</p>
+        <Img fluid={data.imageOne.childImageSharp.fluid} />
       </Layout>
     )}
   />
