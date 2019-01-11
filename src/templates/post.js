6ecadd440f3aed2552689ebaee1992a62b7c5da2
Graham Plata <graham.plata@gmail.com>
2019-01-11 12:20:04 -0500

    minor

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,28 +1,22 @@
-import React, { Fragment } from 'react'
-import { graphql } from 'gatsby'
-import { Header, Container } from 'semantic-ui-react'
-import Layout from '../components/layout'
+import React from "react";
+import { graphql } from "gatsby";
+import { Header, Container } from "semantic-ui-react";
+import Layout from "../components/layout";
 
-const Template = ({ data, pageContext }) => {
-  const { markdownRemark: post } = data
+const Template = ({ data }) => {
+  const { markdownRemark: post } = data;
   return (
     <Layout>
       <Container>
-        <div style={{ marginBottom: 25 }}>
-          <Fragment>
-            <Header as="h2" textAlign="left">
-              <Header.Content>{post.frontmatter.title}</Header.Content>
-            </Header>
-            <Header as="h4" textAlign="left">
-              <Header.Content>{post.frontmatter.date}</Header.Content>
-            </Header>
-          </Fragment>
-        </div>
+        <Header as="h2" textAlign="left">
+          {post.frontmatter.title}
+          <Header.Subheader>{post.frontmatter.date}</Header.Subheader>
+        </Header>
         <Container dangerouslySetInnerHTML={{ __html: post.html }} />
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
 export const postQuery = graphql`
   query($path: String!) {
@@ -35,6 +29,6 @@ export const postQuery = graphql`
       }
     }
   }
-`
+`;
 
-export default Template
+export default Template;
