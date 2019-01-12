88442931c1b6a354ecd15c0bbf6ffa1f07d6c650
Graham Plata <graham.plata@gmail.com>
2019-01-11 23:22:24 -0500

    add lower pagination

--- src/templates/BlogList.js
+++ src/templates/BlogList.js
@@ -73,6 +73,24 @@ export default class BlogList extends React.Component {
               )
             })}
           </Item.Group>
+          <Button.Group compact>
+            <Button as={Link} to={prevPage} disabled={isFirst}>
+              Prev
+            </Button>
+            {Array.from({ length: numPages }, (_, i) => (
+              <Button
+                active={currentPage === i + 1}
+                as={Link}
+                to={`blog/${i === 0 ? '' : i + 1}`}
+                key={i}
+              >
+                {i + 1}
+              </Button>
+            ))}
+            <Button as={Link} to={nextPage} disabled={isLast}>
+              Next
+            </Button>
+          </Button.Group>
         </Container>
       </Layout>
     )
