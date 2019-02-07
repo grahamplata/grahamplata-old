d217371793dd8d9f77e2ea3ca38b1edd98f5941d
Graham Plata <graham.plata@gmail.com>
2019-02-07 15:47:32 -0500

    semantic removed from posts

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,34 +1,22 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { Header, Container, Grid } from 'semantic-ui-react'
-import Layout from '../components/layout'
-import BlogNav from '../components/BlogNav'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import BlogNav from "../components/BlogNav";
 
 const BlogPost = ({ data, pageContext }) => {
-  const { markdownRemark: post } = data
-  const { frontmatter, html } = post
-  const { title, date } = frontmatter
-  const { next, prev } = pageContext
+  const { markdownRemark: post } = data;
+  const { frontmatter, html } = post;
+  const { title, date } = frontmatter;
+  const { next, prev } = pageContext;
   return (
     <Layout>
       <BlogNav next={next} prev={prev} />
-      <Header as="h2" textAlign="left">
-        {title}
-        <Header.Subheader>{date}</Header.Subheader>
-      </Header>
-      <Container>
-        <Grid>
-          <Grid.Column>
-            <Grid.Column>
-              <Container text dangerouslySetInnerHTML={{ __html: html }} />
-            </Grid.Column>
-            <BlogNav next={next} prev={prev} />
-          </Grid.Column>
-        </Grid>
-      </Container>
+      <h2>{title}</h2>
+      <p>{date}</p>
+      <p dangerouslySetInnerHTML={{ __html: html }} />
     </Layout>
-  )
-}
+  );
+};
 
 export const postQuery = graphql`
   query($path: String!) {
@@ -44,6 +32,6 @@ export const postQuery = graphql`
       }
     }
   }
-`
+`;
 
-export default BlogPost
+export default BlogPost;
