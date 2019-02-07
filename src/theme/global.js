e0df2a345c218017b0bcb747560e701fcb229806
Graham Plata <graham.plata@gmail.com>
2019-02-07 18:03:57 -0500

    prism

--- src/theme/global.js
+++ src/theme/global.js
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from 'styled-components'
 import resets from './reset'
 import typography from './typography'
-import prism from './prism'
 import background from './background'
 
 export default createGlobalStyle`
   ${resets}
   ${typography}
-  ${prism}
   ${background}
   `
