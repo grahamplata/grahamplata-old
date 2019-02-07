9093ff27ca7f89d6dcc09db0375c1ae04be5c2b5
Graham Plata <graham.plata@gmail.com>
2019-02-07 14:50:23 -0500

    add tilde

--- src/theme/global.js
+++ src/theme/global.js
@@ -1,12 +1,12 @@
-import { createGlobalStyle } from 'styled-components'
-import resets from 'theme/reset'
-import typography from 'theme/typography'
-import prism from 'theme/prism'
-import background from 'theme/background'
-
+import { createGlobalStyle } from "styled-components";
+import resets from "theme/reset";
+import typography from "theme/typography";
+import prism from "theme/prism";
+import background from "theme/background";
 
 export default createGlobalStyle`
   ${resets}
   ${typography}
   ${prism}
   ${background}
+  `;
