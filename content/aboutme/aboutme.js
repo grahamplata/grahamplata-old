ce6287e414dfb41b36c0aa5b4d90c7976a2e1dc6
Graham Plata <graham.plata@gmail.com>
2019-02-14 20:08:49 -0500

    move about content to static

---
+++ content/aboutme/aboutme.js
@@ -0,0 +1,30 @@
+export const skills = [
+  {
+    skill: "Welding",
+    description: "I want to restore and work on cars eventually."
+  },
+  {
+    skill: "Spirited Driving",
+    description: "I want to learn to Drift or Autocross."
+  },
+  { skill: "Group Riding", description: "Long distance Cycling Rides" },
+  { skill: "Go", description: 'fmt.Println("Hello, Go")' }
+];
+
+export const places = [
+  {
+    place: "Iceland",
+    description:
+      "Iceland is the most unique and exciting place on the planet. You go to Iceland to have adventures and see natural beauty."
+  },
+  {
+    place: "New Zealand",
+    description:
+      "Much like Iceland New Zealand has an amazing range of breathtaking scenery but from subtropical forests to snow-covered mountains."
+  },
+  {
+    place: "Pikes Peak International Hill Climb",
+    description:
+      "The Pikes Peak International Hill Climb, also known as The Race to the Clouds, is an annual automobile and motorcycle hillclimb to the summit of Pikes Peak in Colorado, USA."
+  }
+];
